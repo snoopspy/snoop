8930d4e892387a6ff7128c4a1f4da922effedd0a
gilgil <admin@gilgil.net>
2015-01-26 16:06:04 +0900

    Remove snoop_all.pro

--- _build.sh
+++ _build.sh
@@ -1,37 +1,14 @@
 # ---------------------------------------------------------------------------
-# snoop
+# lib
 # ---------------------------------------------------------------------------
-
-# snoopd.lib
 cd lib
-qmake "CONFIG+=debug" "QT-=gui"
-make
-make clean
-cd ..
-
-# snoopd_gui.lib
-cd lib
-qmake "CONFIG+=debug" "QT+=gui" "QT+=widgets"
-make
-make clean
-cd ..
-
-# snoop.lib
-cd lib
-qmake "CONFIG+=release" "QT-=gui"
-make
-make clean
-cd ..
-
-# snoop_gui.lib
-cd lib
-qmake "CONFIG+=release" "QT+=gui" "QT+=widgets"
-make
-# make clean
+make release -f make.file
 cd ..
 
 # ---------------------------------------------------------------------------
 # app
 # ---------------------------------------------------------------------------
-qmake
+cd app
+qmake "CONFIG+=release"
 make
+
